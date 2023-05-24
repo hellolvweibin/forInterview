f68d36935430c949540476576c9a50433f9ac897
Levi_Bee <279369954@qq.com>
2023-05-24 21:04:13 +0800

    feat: 面试工程init🍳

---
+++ src/main/java/com/lv/Main.java
@@ -0,0 +1,11 @@
+package com.lv;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+    }
+}
---
+++ src/main/java/com/lv/javase/se01.java
@@ -0,0 +1,10 @@
+package com.lv.javase;
+
+/**
+ * @Project ：lv-RPC
+ * @Author ：Levi_Bee
+ * @Date ：2023/3/30 15:39
+ * @description ：自动类型转换
+ */
+public class se01 {
+}
