c9346bd3e46b221404ebd64ffc7b6d5103c8934f
Levi_Bee <279369954@qq.com>
2023-05-25 09:19:40 +0800

    feat: 日志框架完成🍳

--- src/main/java/com/lv/javase/se01.java
+++ src/main/java/com/lv/javase/se01.java
@@ -1,10 +1,20 @@
 package com.lv.javase;
 
+
+import lombok.extern.slf4j.Slf4j;
+
 /**
  * @Project ：lv-RPC
  * @Author ：Levi_Bee
  * @Date ：2023/3/30 15:39
  * @description ：自动类型转换
  */
+@Slf4j
 public class se01 {
+    public static void main(String[] args) {
+        int i = 0;
+        log.info("测试{}", i);
+
+        System.out.println("11");
+    }
 }
